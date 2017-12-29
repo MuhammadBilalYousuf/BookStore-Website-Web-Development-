ba668d09682a736c80bdefbd56739a7c5858fe9b
Muhammad Bilal Yousuf <sp14bs0018@maju.edu.pk>
2017-12-30 01:23:21 +0500

    app.js

--- app/app.js
+++ app/app.js
@@ -2,26 +2,39 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
+var io= require('socket.io')();
+
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData',dataFile);
-app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set('view engine','ejs');
+app.set('views','app/views');
+
 app.locals.siteTitle = 'BookStore';
 app.locals.allBookStore = dataFile.BookStore;
 
 app.use(express.static('app/public'));
 app.use(require('./routers/index'));
 app.use(require('./routers/BookStore'));
-
+app.use(require('./routers/feedback'));
+app.use(require('./routers/api'));
+app.use(require('./routers/chat'));
 
 
 
 var Server = app.listen(app.get('port'), function(){
-	console.log('Yahooo web p jao ' +app.get('port'));
+	console.log('listen to port ' +app.get('port'));
+});
+
+io.attach(Server);
+io.on('connection', function(socket) {
+  socket.on('postMessage', function(data) {
+    io.emit('updateMessages', data);
+  });
 });
 
 reload(Server, app);
+
 
 
 // var http = require('http');
